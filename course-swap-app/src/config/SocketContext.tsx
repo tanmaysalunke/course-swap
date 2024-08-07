06d20e1184fe474431df0e9d1823d1d14b6a7bdc
tanmaysalunke <tanmay442000@gmail.com>
2024-08-07 14:04:28 -0700

    check if socket connects

--- course-swap-app/src/config/SocketContext.tsx
+++ course-swap-app/src/config/SocketContext.tsx
@@ -56,7 +56,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   const userEmail = user?.email;
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000", {
+    const newSocket = io("http://backend:5000", {
       transports: ["websocket", "polling"],
       reconnection: true,
       reconnectionAttempts: 5,
