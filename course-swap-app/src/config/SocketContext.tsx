d7cbb815729a738e265412a3952dad0c72e6cdac
tanmaysalunke <tanmay442000@gmail.com>
2024-08-08 23:59:24 -0700

    check socket

--- course-swap-app/src/config/SocketContext.tsx
+++ course-swap-app/src/config/SocketContext.tsx
@@ -56,7 +56,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   const userEmail = user?.email;
 
   useEffect(() => {
-    const newSocket = io("ws://54.67.32.217/socket.io/", {
+    const newSocket = io("http://54.67.32.217/socket.io/", {
       transports: ["websocket", "polling"],
       reconnection: true,
       reconnectionAttempts: 5,
