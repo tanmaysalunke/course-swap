ccde2c55037e2181a512f139f03fb641ff5b349c
tanmaysalunke <tanmay442000@gmail.com>
2024-08-10 15:30:32 -0700

    check with hashrouter

--- course-swap-app/src/App.tsx
+++ course-swap-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./config/Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { AuthProvider, useAuth } from "./config/AuthContext";
